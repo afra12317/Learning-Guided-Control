ccbcd61cbe745f9f58d02673dc54f29e92051e49
afra12317 <haiyuechen2002@gmail.com>
2025-04-12 13:06:07 -0400

    fixed speed

--- build/mppi_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/flo/lab_ws/src/Learning-Guided-Control-MPPI
-# Build directory: /home/flo/lab_ws/src/Learning-Guided-Control-MPPI/build/mppi_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
